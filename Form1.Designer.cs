886acaf60d6964e17b9b37a56705c05e1e886f33
aeonftw <cioncu_ionut@yahoo.com>
2024-05-03 21:07:05 +0300

    Update old repo

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Practica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_number = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(337, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Number";
+            // 
+            // txt_number
+            // 
+            this.txt_number.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_number.Location = new System.Drawing.Point(383, 322);
+            this.txt_number.Name = "txt_number";
+            this.txt_number.Size = new System.Drawing.Size(104, 20);
+            this.txt_number.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(337, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Time";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(159, 325);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Name";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_name.Location = new System.Drawing.Point(196, 322);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(104, 20);
+            this.txt_name.TabIndex = 2;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_id.Location = new System.Drawing.Point(196, 271);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(104, 20);
+            this.txt_id.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(159, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Id";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = global::Practica.Properties.Resources.digital_technology_concept_background_52683_35037;
+            this.button2.Location = new System.Drawing.Point(383, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Add Provider";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(384, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 26;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = global::Practica.Properties.Resources.digital_technology_concept_background_52683_35037;
+            this.button1.Location = new System.Drawing.Point(196, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Practica.Properties.Resources.image__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(196, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(290, 239);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Practica.Properties.Resources.digital_technology_concept_background_52683_35037;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(688, 483);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_number);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_name);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Over500";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_number;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Insert()
+        {
+
+            label5.Text = String.Empty;
+            Stopwatch sw = new Stopwatch();
+            using (Over5000Entities db = new Over5000Entities())
+            {
+                db.Configuration.AutoDetectChangesEnabled = false;
+                sw.Start();
+
+                int total = Convert.ToInt32(txt_number.Text);
+
+                for (int i = 1; i < total + 1; i++)
+                {
+                    
+
+                    
+
+                    using (var context = new Over5000Entities())
+                    {
+                        var ids = db.Set<Echipamente>();
+                        ids.Add(new Echipamente
+                        {
+                            nume_echipament = String.Format("Echipament {0}", i),
+                            id_producator = int.Parse(txt_id.Text)
+                        });
+                    }
+
+                    Echipamente obj2 = new Echipamente();
+
+                    
+
+                }
+                db.SaveChanges();
+                sw.Stop();
+                label5.Text = string.Format("{0:hh\\:mm\\:ss\\:fff}", sw.Elapsed);
+            }
+
+        }
+
+        public void InsertProvider()
+        {
+            try
+            {
+
+                label5.Text = String.Empty;
+                Stopwatch sw = new Stopwatch();
+                using (Over5000Entities db = new Over5000Entities())
+                {
+                    db.Configuration.AutoDetectChangesEnabled = false;
+                    sw.Start();
+
+                    Producatori obj1 = new Producatori();
+
+                    obj1.nume_producator = txt_name.Text;
+                    db.Producatoris.Add(obj1);
+
+                    db.SaveChanges();
+                    sw.Stop();
+                    label5.Text = string.Format("{0:hh\\:mm\\:ss\\:fff}", sw.Elapsed);
+
+                }
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Insert Failed");
+
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Insert();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InsertProvider();
+        }
+    }
+}
